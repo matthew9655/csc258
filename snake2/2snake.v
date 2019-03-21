9c89a59458ec391e931a4b23b6b8c8e80c670313
dino <dino.li@mail.utoronto.ca>
2019-03-20 20:59:40 -0400

    Milestone1

--- snake2/2snake.v
+++ snake2/2snake.v
@@ -7,13 +7,15 @@
 //LEDR displays result
 //HEX0 & HEX1 also displays result
 
-module snake_2(SW, KEY, CLOCK_50, VGA_HS, VGA_VS, VGA_BLANK,VGA_SYNC, VGA_CLK, VGA_R, VGA_G, VGA_B);
+module snake_2(SW, KEY, CLOCK_50, VGA_HS, VGA_VS, VGA_BLANK,VGA_SYNC, VGA_CLK, VGA_R, VGA_G, VGA_B, PS2_CLK, PS2_DAT);
     input [9:0] SW;
     input [3:0] KEY;
     input CLOCK_50;
     output VGA_HS, VGA_VS, VGA_BLANK,VGA_SYNC, VGA_CLK;
 	 output [9:0] VGA_R, VGA_G, VGA_B;
-
+	 inout PS2_CLK, PS2_DAT;
+	 
+	 
     wire resetn;
     wire go;
 	 wire draw;
@@ -34,7 +36,9 @@ module snake_2(SW, KEY, CLOCK_50, VGA_HS, VGA_VS, VGA_BLANK,VGA_SYNC, VGA_CLK, V
 		  .VGA_CLK(VGA_CLK),
 		  .VGA_R(VGA_R),
 		  .VGA_G(VGA_G),
-		  .VGA_B(VGA_B)
+		  .VGA_B(VGA_B),
+		  .PS2_CLK(PS2_CLK),
+		  .PS2_DAT(PS2_DAT)
     );
 
 endmodule
@@ -43,7 +47,7 @@ module part2(
     input clk,
     input resetn,
     input go,
-	 input draw, stop, delay,
+	 input draw,
 	 input [2:0] color,
 	 output VGA_HS, VGA_VS, VGA_BLANK,VGA_SYNC, VGA_CLK,
 	 output [9:0] VGA_R, VGA_G, VGA_B,
@@ -57,7 +61,7 @@ module part2(
 	 wire [6:0] food_y;
 	 
 	 // wires for moving the bit
-    wire black, move, ld_c, enable, plot;
+    wire black, move, ld_c, enable, plot, stop, delay;
 	 wire left, right, up, down;
 	 wire [7:0] x_out; 
 	 wire [6:0] y_out;
@@ -74,16 +78,16 @@ module part2(
 			.cout(delay),
 			.reset(reset),
 			.clk(clk),
-			.d({4'b0, 24'd12500000})
+			.d({4'b0, 24'd12500000}) 
 	 );
 	 
-	 
-	 food_gen(
-			.clk(clk),
-			.gen(gen),
-			.randomX(food_x),
-			.randomY(food_y)
-	 );
+//	 
+//	 food_gen(
+//			.clk(clk),
+//			.gen(gen),
+//			.randomX(food_x),
+//			.randomY(food_y)
+//	 );
 	 
 	 
 	 vga_adapter a0(
@@ -114,11 +118,10 @@ module part2(
         .resetn(resetn),
         .go(go),
 		  .draw(draw),
-		  .stop(stop_counter),
+		  .stop(stop),
 		  .delay(delay),
-        .black(black),
+		  
         .move(move),
-        .ld_c(ld_c),
         .enable(enable),
         .plot(plot)
         
@@ -135,12 +138,11 @@ module part2(
 		  .move(move),
 		  .food_x(food_x),
 		  .food_y(food_y),
-        .enable(enable),
-        .ld_c(ld_c), 
+        .enable(enable), 
 		  .colour(color),
 		  
 		  .stop(stop),
-		  .gen_food(gen)
+		  .food_gen(gen),
         .x_out(x_out),
 		  .y_out(y_out),
 		  .c_out(c_out)
@@ -174,26 +176,28 @@ module control(
 	 input draw,
 	 input stop,
 	 input delay,
-	 input food_gen
+	 input food_gen,
 	 
-
-    output reg  black, move, ld_c, enable, plot
-    );
+	 output reg move, enable, plot
+	 output reg colour[2:0];
+	 );
+	
+	
+    //output reg  black, move, ld_c, enable, plot;
+   
 
     reg [3:0] current_state, next_state; 
     
     localparam 
-                S_LOAD_C        = 4'd1,
-                S_LOAD_C_WAIT   = 4'd2,
-					 DRAW            = 4'd3,
-					 DRAW_WAIT       = 4'd4,
-					 BLACK           = 4'd5,
-                S_CYCLE_0       = 4'd6,
-					 S_CYCLE_1       = 4'd7,
-					 MOVE				  = 4'd8,
-					 S_CYCLE_2       = 4'd9,
-					 S_CYCLE_3       = 4'd10,
-					 WAIT            = 4'd11;
+					 START           = 4'd0,
+					 START_WAIT		  = 4'd1,
+					 DRAW            = 4'd2,
+					 DRAW1			  = 4'd3,
+					 WAIT            = 4'd4,
+					 BLACK			  = 4'd5,
+					 BLACK1		     = 4'd6,
+					 MOVE				  = 4'd7;
+			
 					 
 					 
     
@@ -201,22 +205,15 @@ module control(
     always@(*)
     begin: state_table 
             case (current_state)
-                S_LOAD_C: next_state = go ? S_LOAD_C_WAIT : S_LOAD_C; // Loop in current state until value is input
-                S_LOAD_C_WAIT: next_state = go ? S_LOAD_C_WAIT : DRAW; // Loop in current state until go signal goes low
-					 DRAW: next_state = draw ? DRAW_WAIT : DRAW;
-					 DRAW_WAIT: next_state = draw ? DRAW_WAIT : BLACK;
-					 BLACK: next_state = S_CYCLE_0;
-                S_CYCLE_0: next_state = S_CYCLE_1;
-					 S_CYCLE_1: next_state = stop ? MOVE : S_CYCLE_0; 
-					 MOVE: next_state = S_CYCLE_2;
-					 S_CYCLE_2: next_state = S_CYCLE_3;
-					 S_CYCLE_3: next_state = stop ? FOOD_WAIT : S_CYCLE_2;
-					 FOOD_WAIT: next_state = food_gen ? FOOD : WAIT;
-					 FOOD: next_state = S_CYCLE_4;
-					 S_CYCLE_4: next_state = S_CYCLE_5;
-					 S_CYCLE_5: next_state = stop ? WAIT : S_CYCLE_4;
-					 WAIT: next_state = delay ? BLACK : WAIT;
-            default:     next_state = S_LOAD_C;
+					 START: next_state = draw ? DRAW_WAIT : DRAW;
+					 START_WAIT: next_state = draw ? DRAW_WAIT : DRAW;
+                DRAW: next_state = DRAW1;
+					 DRAW1: next_state = stop ? WAIT : DRAW; 
+					 WAIT: next_state = delay ? DRAW2 : WAIT
+					 BLACK: next_state = BLACK1;
+					 BLACK1: next_state = stop ? MOVE : BLACK;
+					 MOVE: next_state = DRAW;
+            default:     next_state = START;
         endcase
     end // state_table
    
@@ -228,40 +225,42 @@ module control(
         ld_c = 1'b0;
 		  enable = 1'b0;
 		  plot = 1'b0;
-		  black = 1'b0;
+		  colour = 3'b000;
 		  move = 1'b0;
 
 
         case (current_state)
-            S_LOAD_C: begin
-                ld_c = 1'b1;
-                end
-				BLACK: begin 
-					black = 1'b1;
-					end
-            S_CYCLE_0: begin 
+            DRAW: 
+				begin
+					enable = 1'b1;
+					plot = 1'b1;
+					colour = 3b'100;
+            end
+				DRAW1: 
+				begin 
 					 enable = 1'b1;
 					 plot = 1'b1;
-					 end
-				S_CYCLE_1: begin 
+				end
+				WAIT: 
+				begin 
+					 colour = 3b'111;
+				end	
+				BLACK: 
+				begin 
 					 enable = 1'b1;
 					 plot = 1'b1;
-					 end
-				MOVE: begin
+				end
+				BLACK1: 
+				begin 
+					 enable = 1'b1;
+					 plot = 1'b1;
+				end
+				MOVE: 
+				begin
 					 move = 1'b1;
-					 ld_c = 1'b1;
 					 enable = 1'b0;
 					 plot = 1'b0;
-					 end
-				S_CYCLE_2: begin 
-					 enable = 1'b1;
-					 plot = 1'b1;
-					 end
-				S_CYCLE_3: begin 
-					 enable = 1'b1;
-					 plot = 1'b1;
-					 end
-					 
+			   end
         endcase
     end // enable_signals
    
@@ -269,7 +268,7 @@ module control(
     always@(posedge clk)
     begin: state_FFs
         if(!resetn)
-            current_state <= S_LOAD_C;
+            current_state <= START;
         else
             current_state <= next_state;
     end // state_FFS
@@ -281,14 +280,12 @@ module datapath(
     input resetn, enable,
 	 input left, right, up, down,// if y_dir is 1, go down else up. if x_dir is 1, go right, else left.
     input [2:0] colour,
-    input ld_c,
-	 input move, // if move is high, move x y based on x_dir, y_dir
-	 input black, // colour the pixel black if black is high
+	 input move, // if move is high, move x y based on x_dir, y_dir // colour the pixel black if black is high
 	 
 	 // food 
 	 input [7:0] food_x,
 	 input [6:0] food_y,
-	 output reg gen_food,
+	 output reg food_gen,
 	 
 	 
     output [8:0] x_out,
@@ -306,39 +303,15 @@ module datapath(
 	 
 	 // registers for directions
 	 reg x_dir, y_dir;
-	 reg ver, hor;
-	 
 	 
     // registers for regc
 	 
-    always @ (posedge clk) 
-	 begin
-        if (!resetn) 
-		  begin
-				regc <= 3'b0;
-				
-        end
-        else 
-		  begin
-				if (ld_c == 1)
-				begin
-					regc <= colour;
-				end
-				else if (black == 1)
-				begin 
-					regc <= 3'b0;
-				end
-        end
-		  
-    end
 	 
 	 // setting the logic direction bits
 	 always @ (posedge clk) 
 	 begin 
 		if (!resetn)
 		begin
-		    ver <= 1'b0;
-			 hor <= 1'b0;
 			 x_dir <= 1'b0;
 			 y_dir <= 1'b0;
 			 
@@ -347,26 +320,18 @@ module datapath(
 		begin 
 			if (left == 1)
 			begin 
-				hor <= 1'b1;
-				ver <= 1'b0;
 				x_dir <= 1'b0;
 			end
 			else if (right == 1)
 			begin 
-				hor <= 1'b1;
-				ver <= 1'b0;
 				x_dir <= 1'b1;
 			end
 			else if (up == 1)
 			begin 
-				ver <= 1'b1;
-				hor <= 1'b0;
 				y_dir <= 1'b0;
 			end
 			else if (down == 1)
 			begin
-				ver <= 1'b1;
-				hor <= 1'b0;
 				y_dir <= 1'b1;
 			end
 		end
@@ -379,28 +344,28 @@ module datapath(
 	 begin 
 		if (!resetn)
 		begin
-		    regx <= 8'd80;
-			 regy <= 7'd60;
+		    regx <= 8'b0;
+			 regy <= 7'b0;
 		end
 		
 		else 
 		begin
 			if (move == 1) 
 			begin
-				if (x_dir == 1 && ver == 1)
+				if (x_dir == 1)
 				begin
 					regx <= regx + 1;
 				end
-				else if (x_dir == 0 && ver == 1)
+				else if (x_dir == 0)
 				begin
 					regx <= regx - 1;
 				end
 				
-				if (y_dir == 1 && hor == 1)
+				if (y_dir == 1)
 				begin 
 					regy <= regy + 1;
 				end
-				else if (y_dir == 0 && hor == 1)
+				else if (y_dir == 0)
 				begin 
 					regy <= regy - 1;
 				end
@@ -438,13 +403,13 @@ module datapath(
 	 assign c_out = regc;
 	 
 	 // checking for food 
-	 always @ (posedge clk) 
-	 begin
-		if (regx == food_x) && (regy == food_y)
-			begin 
-			gen_food <= 1'b1;
-			end
-	 end
+//	 always @ (posedge clk) 
+//	 begin
+//		if ((regx == food_x) && (regy == food_y))
+//			begin 
+//			food_gen <= 1'b1;
+//			end
+//	 end
 
     
 endmodule
@@ -480,81 +445,81 @@ module RateDivider(cout, reset, clk, d) ; // need to take into account of the fr
 endmodule
 
 
-module food_gen (clk, gen, randomX, randomY);
-	input clk, gen;
-	output reg [9:0] randomX;
-	output reg [8:0] randomY;
-
-	reg [6:0] x = 10; //.5 -> 0 - 127 -> 62
-	reg [6:0] y = 10; //.5 -> 0 - 127 -> 46
+//module food_gen (clk, gen, randomX, randomY);
+//	input clk, gen;
+//	output reg [9:0] randomX;
+//	output reg [8:0] randomY;
+//
+//	reg [6:0] x = 10; //.5 -> 0 - 127 -> 62
+//	reg [6:0] y = 10; //.5 -> 0 - 127 -> 46
+//	
+//	always@(posedge clk)
+//		x <= x + 4;
+//		
+//	always@(posedge clk)
+//		y <= y + 2;
+//	
+//	always@(posedge clk)
+//	begin
+//		if (gen == 1)
+//		begin
+//			if(x > 46)
+//			begin
+//				randomX <= ((x % 46) + 1) * 10;
+//			end
+//			else if (x < 5)
+//			begin
+//				randomX <= ((x + 1) * 4) * 10;
+//			end
+//			else
+//			begin
+//				randomX <= (x * 10);
+//			end
+//		end
+//	end
+//	
+//	always@(posedge clk)
+//	begin
+//		if (gen == 1)
+//		begin
+//			if(y > 46)
+//			begin
+//				randomY <= ((y % 46) + 1) * 10;
+//			end
+//			else if (y < 5)
+//			begin
+//				randomY <= ((y + 1) * 3) * 10;
+//			end
+//			else
+//			begin
+//				randomY <= (y * 10);
+//			end
+//		end
+//	end
+//	endmodule 
 	
-	always@(posedge clk)
-		x <= x + 4;
-		
-	always@(posedge clk)
-		y <= y + 2;
-	
-	always@(posedge clk)
-	begin
-		if (gen == 1)
-		begin
-			if(x > 46)
-			begin
-				randomX <= ((x % 46) + 1) * 10;
-			end
-			else if (x < 5)
-			begin
-				randomX <= ((x + 1) * 4) * 10;
-			end
-			else
-			begin
-				randomX <= (x * 10);
-			end
-		end
-	end
-	
-	always@(posedge clk)
-	begin
-		if (gen == 1)
-		begin
-			if(y > 46)
-			begin
-				randomY <= ((y % 46) + 1) * 10;
-			end
-			else if (y < 5)
-			begin
-				randomY <= ((y + 1) * 3) * 10;
-			end
-			else
-			begin
-				randomY <= (y * 10);
-			end
-		end
-	end
-	endmodule 
-	
-module foodTest(CLOCK_50, snakeX, snakeY, foodX, foodY, outX, outY);
-	input CLOCK_50;
-	 input[9:0] snakeX, foodX;
-	 input[8:0] snakeY, foodY;
-	 output reg[9:0] outX;
-	 output reg[8:0] outY;
-	 wire[9:0] x;
-	 wire[8:0] y;
-	 random_generator ran(CLOCK_50, x, y);
-	 assign regenerate_food = (snakeX == foodX)&&(snakeY == foodY);
-	 always @(posedge CLOCK_50)
-	 begin
-		if(regenerate_food == 1'b1)
-		begin
-			outX <= x;
-			outY <= y;
-		end
-		else
-		begin
-			outX <= foodX;
-			outY <= foodY;
-		end
-	end
-	endmodule
+//module foodTest(CLOCK_50, snakeX, snakeY, foodX, foodY, outX, outY);
+//	input CLOCK_50;
+//	 input[9:0] snakeX, foodX;
+//	 input[8:0] snakeY, foodY;
+//	 output reg[9:0] outX;
+//	 output reg[8:0] outY;
+//	 wire[9:0] x;
+//	 wire[8:0] y;
+//	 random_generator ran(CLOCK_50, x, y);
+//	 assign regenerate_food = (snakeX == foodX)&&(snakeY == foodY);
+//	 always @(posedge CLOCK_50)
+//	 begin
+//		if(regenerate_food == 1'b1)
+//		begin
+//			outX <= x;
+//			outY <= y;
+//		end
+//		else
+//		begin
+//			outX <= foodX;
+//			outY <= foodY;
+//		end
+//	end
+//	endmodule
 
