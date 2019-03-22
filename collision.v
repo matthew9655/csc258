28c4b2374fa60517e49d4c76286e801e4f25ef7e
Li Ao <liao6@teach.cs.toronto.edu>
2019-03-22 11:45:40 -0400

    Fix the boarder

--- collision.v
+++ collision.v
@@ -1,13 +1,28 @@
-module collision_test(clk, regx, regy, collision);
+module collision_test(clk, regx, regy, direction,collision);
 	input clk;
 	input [7:0]regx;
 	input [6:0]regy;
+	input [1:0]direction;
 	output reg [0:0]collision;
 	reg [0:0] boarder;
 	always @(posedge clk)
 	begin
-	boarder <= ((regx == 0) || (regx == 159) || (regy == 0) || (regy == 119));
+	case (direction)
+	if(direction == 2'd0)
+		begin
+			collision <= (regx == 0);
+		end
+	else if(direction == 2'd1)
+		begin
+			collision <= (regx  == 159);
+		end
+	else if (direction == 2'd2)
+		begin
+			collision <= (regy == 0);
+		end
+	else
+		begin
+			collision <= (regy == 119);
+		end
 	end 
-	always @(*)
-	collision <= boarder;
 endmodule
