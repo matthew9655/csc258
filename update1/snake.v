e9e0c5ec9461ba36cfeb7b999e9f17d8722de83d
dino <dino.li@mail.utoronto.ca>
2019-03-27 20:57:07 -0400

    Milestone 2 done

--- update1/snake.v
+++ update1/snake.v
@@ -1,44 +1,11 @@
 //KEY[0] active low reset
 //KEY[1] go signal
 
-
-module snake_2(SW, KEY, CLOCK_50, VGA_HS, VGA_VS, VGA_BLANK,VGA_SYNC, VGA_CLK, VGA_R, VGA_G, VGA_B, PS2_CLK, PS2_DAT);
-	 input [9:0] SW;
+module snake(SW, KEY, CLOCK_50, VGA_HS, VGA_VS, VGA_BLANK,VGA_SYNC, VGA_CLK, VGA_R, VGA_G, VGA_B, PS2_CLK, PS2_DAT);
     input [3:0] KEY;
+	 input [9:0] SW;
     input CLOCK_50;
-    output VGA_HS, VGA_VS, VGA_BLANK,VGA_SYNC, VGA_CLK;
-	 output [9:0] VGA_R, VGA_G, VGA_B;
 	 inout PS2_CLK, PS2_DAT;
-	 
-	 
-    wire resetn;
-    assign resetn = KEY[0];
-
-    part2 u0(
-        .clk(CLOCK_50),
-        .resetn(resetn),
-		  .l(SW[0]),
-		  .r(SW[1]),
-		  .u(SW[2]),
-		  .d(SW[3]),
-        .VGA_HS(VGA_HS),
-		  .VGA_VS(VGA_VS),
-		  .VGA_BLANK(VGA_BLANK),
-		  .VGA_SYNC(VGA_SYNC),
-		  .VGA_CLK(VGA_CLK),
-		  .VGA_R(VGA_R),
-		  .VGA_G(VGA_G),
-		  .VGA_B(VGA_B),
-		  .PS2_CLK(PS2_CLK),
-		  .PS2_DAT(PS2_DAT)
-    );
-
-endmodule
-
-module snake(SW, KEY, CLOCK_50, VGA_HS, VGA_VS, VGA_BLANK,VGA_SYNC, VGA_CLK, VGA_R, VGA_G, VGA_B);
-    input [3:0] KEY;
-	 input [9:0] SW;
-    input CLOCK_50;
     output VGA_HS, VGA_VS, VGA_BLANK,VGA_SYNC, VGA_CLK;
 	 output [9:0] VGA_R, VGA_G, VGA_B;
 	
@@ -48,9 +15,13 @@ module snake(SW, KEY, CLOCK_50, VGA_HS, VGA_VS, VGA_BLANK,VGA_SYNC, VGA_CLK, VGA
 	 wire [2:0] colour;
 	 wire plot;
 	 
+	 wire kleft, kright, kup, kdown; 
+	 
+	 wire [5:0] not_used;
+	 
 	 
 	 vga_adapter a0(
-			.resetn(SW[9]),
+			.resetn(KEY[0]),
 			.clock(CLOCK_50),
 			.colour(colour),
 			.x(x_out), 
@@ -71,69 +42,65 @@ module snake(SW, KEY, CLOCK_50, VGA_HS, VGA_VS, VGA_BLANK,VGA_SYNC, VGA_CLK, VGA
 		   defparam a0.BITS_PER_COLOUR_CHANNEL = 1;
          defparam a0.BACKGROUND_IMAGE = "black.mif";
 			
-	 combined c0(CLOCK_50, SW[9], SW[0], SW[1], SW[2], SW[3], x_out, y_out, colour, plot);
+			
+	 keyboard_tracker #(.PULSE_OR_HOLD(1)) KB0 (
+			.clock(CLOCK_50),
+			.reset(KEY[0]),
+			.PS2_CLK(PS2_CLK),
+			.PS2_DAT(PS2_DAT),
+			.w(not_used[5]),
+			.a(not_used[4]),
+			.s(not_used[3]),
+			.d(not_used[2]),
+			.space(not_used[1]),
+			.enter(not_used[0]),
+			.left(kleft),
+			.right(kright),
+			.up(kup),
+			.down(kdown)	 
+	);
+			
+	 combined c0(CLOCK_50, KEY[0], KEY[1], kleft, kright, kup, kdown, x_out, y_out, colour, plot);
 	
-endmodule
+endmodule      
 
+
+module combined(clk, resetn, start, l, r, u, d, x_out, y_out, colour, plot);
+	input clk, resetn, start;
+	input l, r, u, d;
 	
-
-module part2(
-    input clk,
-    input resetn,
-	 input l, r, u, d,
-	 output VGA_HS, VGA_VS, VGA_BLANK,VGA_SYNC, VGA_CLK,
-	 output [9:0] VGA_R, VGA_G, VGA_B,
-	 inout PS2_CLK, PS2_DAT
-    );
-	 
-	 // wires for control
-	 wire plot; 
-	 wire [1:0] direction;
-	 wire [2:0] state;
-	 
-	 // wires for datapath
-	 wire [2:0] colour;
-	 wire [7:0] x_out; 
-	 wire [6:0] y_out;
-	 
-	 wire [1:0] dir;
-	  
-	 wire kleft, kright, kup, kdown;
-	 wire not_used[5:0];
-	 
-	 wire enable_d;
-	 
-	 wire gen;
-	 wire [7:0] foodx;
-	 wire [6:0] foody;
-	 
-	 vga_adapter a0(
-			.resetn(resetn),
-			.clock(clk),
-			.colour(colour),
-			.x(x_out), 
-			.y(y_out),
-			.plot(plot),
-			/* Signals for the DAC to drive the monitor. */
-			.VGA_R(VGA_R),
-			.VGA_G(VGA_G),
-			.VGA_B(VGA_B),
-			.VGA_HS(VGA_HS),
-			.VGA_VS(VGA_VS),
-			.VGA_BLANK(VGA_BLANK),
-			.VGA_SYNC(VGA_SYNC),
-			.VGA_CLK(VGA_CLK)
-			);
-			defparam a0.RESOLUTION = "160x120";
-		   defparam a0.MONOCHROME = "FALSE";
-		   defparam a0.BITS_PER_COLOUR_CHANNEL = 1;
-         defparam a0.BACKGROUND_IMAGE = "black.mif";
-		
-	 
-    control C0(
+	output [7:0] x_out; 
+	output [6:0] y_out;
+	output [2:0] colour;
+	output plot;
+	
+	wire delay;
+	wire [1:0] direction;
+	wire [3:0] state;
+	wire [1:0] dir;
+	wire [7:0] randx;
+	wire [6:0] randy;
+	wire food_gen;
+	wire [27:0] timer;
+	
+	
+	time_controller t0(
+		.out(timer),
+		.select(select)
+	);
+	
+	RateDivider r0(
+		  .cout(delay),
+		  .resetn(resetn),
+		  .clk(clk),
+		  .d(timer)
+	);
+	
+	control C0(
         .clk(clk),
         .resetn(resetn),
 		  .delay(delay),
+		  .start(start),
 		  .dir(dir),
 		  
 		  
@@ -148,10 +115,14 @@ module part2(
         .resetn(resetn),
 		  .direction(direction),
 		  .state(state),
-
+		  .randx(randx),
+		  .randy(randy),
+		  
         .x_out(x_out),
 		  .y_out(y_out),
-		  .colour(colour)
+		  .colour(colour),
+		  .food_gen(food_gen),
+		  .select(select)
     );
 	 
 
@@ -163,62 +134,43 @@ module part2(
 			.out(dir)
 	 );
 	 
-//	 keyboard_tracker #(.PULSE_OR_HOLD(1)) KB0 (
-//			.clock(clk),
-//			.reset(reset),
-//			.PS2_CLK(PS2_CLK),
-//			.PS2_DAT(PS2_DAT),
-//			.w(not_used[5]),
-//			.a(not_used[4]),
-//			.s(not_used[3]),
-//			.d(not_used[2]),
-//			.space(not_used[1]),
-//			.enter(not_used[0]),
-//			.left(kleft),
-//			.right(kright),
-//			.up(kup),
-//			.down(kdown)
-//	 );
-	
-	 RateDivider r0(
-			.cout(delay),
-			.resetn(reset),
+	 
+	 food_gen fg0(
 			.clk(clk),
-			.d(28'd12499999)
+			.food_gen(food_gen),
+			.randomX(randx),
+			.randomY(randy)
 	 );
 	 
-//	 food_gen fg0(
-//			.clk(clk),
-//			.gen(gen),
-//			.randomX(foodx),
-//			.randomY(foody)
-//	 );
-                
- endmodule        
+	 
+endmodule
                 
 
 module control(
     input clk,
 	 input delay,
     input resetn,
+	 input start,
 	 input [1:0] dir, // 0 for left, 1 for right, 2 for up, 3 for down
 	 
 	 output reg plot,
 	 output reg [1:0] direction,
-	 output [2:0] state
+	 output [3:0] state
 	 
 	 );
 	
-    reg [2:0] current_state, next_state; 
+    reg [3:0] current_state, next_state; 
     
     localparam  
-					 CLEAR_WAIT			  = 3'd0,
-					 CLEAR              = 3'd1,
-					 MOVE_WAIT		     = 3'd2,
-					 MOVE		           = 3'd3,
-					 EAT_WAIT		     = 3'd4,
-					 EAT                = 3'd5,
-					 REPEAT			     = 3'd6;
+					 START				  = 4'd0,
+					 START_WAIT			  = 4'd1,
+					 CLEAR_WAIT			  = 4'd2,
+					 CLEAR              = 4'd3,
+					 MOVE_WAIT		     = 4'd4,
+					 MOVE		           = 4'd5,
+					 EAT_WAIT		     = 4'd6,
+					 EAT                = 4'd7,
+					 REPEAT			     = 4'd8;
 	 
 	 localparam 
 					LEFT = 2'b0,
@@ -233,6 +185,8 @@ module control(
     always@(*)
     begin: state_table 
             case (current_state)
+					START: next_state = start ? START : START_WAIT;
+					START_WAIT: next_state = start ? CLEAR_WAIT: START_WAIT;
 					CLEAR_WAIT: next_state = CLEAR;
 					CLEAR: next_state = MOVE_WAIT;
 					MOVE_WAIT: next_state = MOVE;
@@ -240,7 +194,7 @@ module control(
 					EAT_WAIT: next_state = EAT;
 					EAT: next_state = REPEAT;
 					REPEAT: next_state = delay ? CLEAR_WAIT : REPEAT;
-					default: next_state = REPEAT;
+					default: next_state = CLEAR_WAIT;
 				endcase
 	 end
 	 
@@ -275,7 +229,7 @@ module control(
 			begin: state_FFs
 				 if(!resetn)
 				 begin
-					current_state <= REPEAT;
+					current_state <= START;
 					plot <= 1'b0;
 				 end
 				 else
@@ -295,17 +249,22 @@ module datapath(
 	 // moving snake
     input clk, resetn,
 	 input [1:0] direction,
-    input [2:0] state,
-	 
+    input [3:0] state,
+	 input [7:0] randx,
+	 input [6:0] randy,
+	 input [6:0] setupx, setupy;
 	 
 	 
     output reg [7:0] x_out,
 	 output reg [6:0] y_out,
-	 output reg [2:0] colour
+	 output reg [2:0] colour,
+	 output reg food_gen,
+	 output reg [4:0] select,
+	 output reg setup
     );
 	 
 	 reg [5:0] length;
-	 reg food_gen, start;
+	 reg start, over;
 	 reg [7:0] headx, foodx;
 	 reg [6:0] heady, foody;
 	 reg [7:0] bodyx[0:127];
@@ -315,13 +274,16 @@ module datapath(
 	 
 	 
 	 localparam  
-					 CLEAR_WAIT			  = 3'b000,
-					 CLEAR              = 3'b001,
-					 MOVE_WAIT		     = 3'b010,
-					 MOVE		           = 3'b011,
-					 EAT_WAIT		     = 3'b100,
-					 EAT                = 3'b101,
-					 REPEAT			     = 3'b110;
+					 START 				  = 4'b000,
+					 START_WAIT			  = 4'b001,
+					 
+					 CLEAR_WAIT			  = 4'b010,
+					 CLEAR              = 4'b011,
+					 MOVE_WAIT		     = 4'b100,
+					 MOVE		           = 4'b101,
+					 EAT_WAIT		     = 4'b110,
+					 EAT                = 4'b111,
+					 REPEAT			     = 4'b110;
 	 
 	 localparam 
 					LEFT = 2'b0,
@@ -329,16 +291,6 @@ module datapath(
 					UP = 2'b10,
 					DOWN = 2'b11;
 					
-	 wire [7:0] randx;
-	 wire [6:0] randy;
-	 
-	 food_gen fg0(
-			.clk(clk),
-			.food_gen(food_gen),
-			.randomX(randx),
-			.randomY(randy)
-	 );
-	 
 					
 	 always@(posedge clk)
 	 begin 
@@ -356,25 +308,63 @@ module datapath(
 		food_gen <= 1'b0;
 		length <= 1'b1;
 		start <= 1'b1;
+		over <= 1'b0;
+		select <= 5'b0;
 		end
 		
 		
 		else
 		begin 
 			case (state)
+			START:
+			begin 
+			end
+			START_WAIT:
+			begin 
+			end
+			
 			CLEAR_WAIT:
 			begin 
 				food_gen <= 1'b0;
-				
-				if (direction == 2'b0)
-					headx <= headx - 1'b1;
-				else if (direction == 2'b01)
-					headx <= headx + 1'b1;
-				else if (direction == 2'b10)
-					heady <= heady - 1'b1;
-				else if (direction == 2'b11)
-					heady <= heady + 1'b1;
+				if (direction == LEFT)
+				begin
+					if (headx == 8'b0)
+						over <= 1'b1;
+					else
+						headx <= headx - 1'b1;
+				end
+				else if (direction == RIGHT)
+				begin
+					if(headx == 8'd159)
+						over <= 1'b1;
+					else
+						headx <= headx + 1'b1; 
+				end
+				else if (direction == UP)
+				begin
+					if(heady == 1'b0)
+						over <= 1'b1;
+					else
+						heady <= heady - 1'b1;
+				end
+				else if (direction == DOWN)
+				begin
+					if(heady  == 7'd119)
+						over <= 1'b1;
+					else
+						heady <= heady + 1'b1;
+				end
 			end
+//				if (direction == LEFT)
+//					over <= 1'b1;
+//				else if (
+//					headx <= headx - 1'b1;
+//				else if (direction == RIGHT)
+//					headx <= headx + 1'b1;
+//				else if (direction == UP)
+//					heady <= heady - 1'b1;
+//				else if (direction == DOWN)
+//					heady <= heady + 1'b1;
 			CLEAR:
 			begin
 				if (headx == foodx && heady == foody)
@@ -384,16 +374,44 @@ module datapath(
 				
 				x_out <= bodyx[length - 1];
 				y_out <= bodyy[length - 1];
+				
+				if (over)
+				begin
+				x_out <= 8'b0;
+				y_out <= 7'b0;
+				headx <= 8'b0;
+				heady <= 7'b0;
+				bodyx[0] <= 8'b0;
+				bodyy[0] <= 7'b0;
+				foodx <= 8'd6;
+				foody <= 7'd0;
+				colour <= 3'b000;
+				food_gen <= 1'b0;
+				length <= 1'b1;
+				start <= 1'b1;
+				over <= 1'b0;
+				select <= 4'b0000;
+				end
+					
 			end
 			MOVE_WAIT:
 			begin 
 				if (foodx == headx && foody == heady) 
+				begin
 					length <= length + 1'b1;
+					food_gen <= 1'b1;
+				end
 				
 			end
 			
 			MOVE:
 			begin 
+				if ((length % 4) == 0) 
+				begin 
+					select <= select + 1'b1;
+				end 
+				
+				
 				x_out <= headx;
 				y_out <= heady;
 				colour <= 3'b111;
@@ -404,7 +422,7 @@ module datapath(
 				begin 
 					if (bodyx[i] > 0)
 					begin
-						bodyx[i + 1] <= bodyx[i] - 1'b1;
+						bodyx[i + 1] <= bodyx[i];
 					end 
 					else 
 					begin
@@ -413,7 +431,7 @@ module datapath(
 					
 					if (bodyy[i] > 0)
 					begin
-						bodyy[i + 1] <= bodyy[i] - 1'b1;
+						bodyy[i + 1] <= bodyy[i];
 					end 
 					else 
 					begin
@@ -423,8 +441,6 @@ module datapath(
 			end
 			EAT_WAIT:
 			begin 
-				if (foodx == headx && foody == heady)
-					food_gen <= 1'b1;
 			end
 			EAT:
 			begin 
@@ -438,12 +454,11 @@ module datapath(
 					
 				else if ((foodx == headx && foody == heady))
 				begin
-					
 					foodx <= randx;
 					foody <= randy;
 				
-					x_out <= foodx;
-					y_out <= foody;
+					x_out <= randx;
+					y_out <= randy;
 					colour <= 3'b010;
 				end
 					
@@ -456,35 +471,50 @@ module datapath(
 			 
 endmodule
 
-module counter6(resetn, clk, enable, out) ;
-	input resetn, clk, enable;
-	output out;
+
+module setup(x, y, clk, resetn, enable);
+	input resetn, enable;
+	output reg [6:0] x;
+	output reg [6:0] y;
 	
-	reg [2:0] count;
+	reg [5:0] xcount;
+	reg [5:0] ycount;
 	
-	
-	always @ (posedge clk, negedge resetn) 
-	 begin
-        if (!resetn) 
-		  begin
-            count <= 3'b0; 
+	always@(posedge clk)
+	begin
+		if (!resetn)
+		begin 
+		x <= 7'd48;
+		y <= 7'd28;
+		count <= 6'b0;
+		end
+		
+		else if (enable)
+		begin
+			if (xcount == 6'b111111)
+			begin
+				xcount <= 6'b0;
+				ycount <= ycount + 1'b1;
+			end
+			else 
+			begin 
+				xcount <= xcount + 1'b1;
+			end
+			
+			if (ycount == 6'b111111) 
+				enable <= 1'b0; 
+			else 
+				ycount <= ycount + 1'b1;			
 				
-        end
-        else if (enable)
-		  begin
-           if (count == 3'd6) 
-			  begin
-					count <= 0;
-			  end
-			  else 
-			  begin
-					count <= count + 1;
-			  end
-		  end
-	 end
-	 assign out = (count == 3'd6) ? 1 : 0;
-	 
+		end
+	end
+	
+	assign x = x + xcount;
+	assign y = y + ycount;
+	
 endmodule
+	
+	
 	
 
 module keyboard_reader(left, right, up, down, out);
@@ -512,7 +542,24 @@ module keyboard_reader(left, right, up, down, out);
 	end
 endmodule
 
-		
+
+module time_controller(out, select);
+	output reg [27:0] out; 
+	input [4:0] select; 
+	
+	always @(*)
+	begin 
+			case (select)
+				5'b00000: out = 28'd6400000;
+				5'b00001: out = 28'd4500000;
+				5'b00010: out = 28'd3200000;
+				5'b00011: out = 28'd1666667;
+				5'b00100: out = 28'd866666;
+				default: out = 28'd866666;
+			endcase
+	end 
+endmodule
+
 
 module RateDivider(cout, resetn, clk, d) ; // need to take into account of the frames too
 	input [27:0] d;
@@ -575,61 +622,5 @@ module food_gen (clk, food_gen, randomX, randomY);
 	end
 	
 endmodule 
-
-
-module combined(clk, resetn, l, r, u, d, x_out, y_out, colour, plot);
-	input clk, resetn; 
-	input l, r, u, d;
-	
-	output [7:0] x_out; 
-	output [6:0] y_out;
-	output [2:0] colour;
-	output plot;
-	
-	wire delay;
-	wire [1:0] direction;
-	wire [2:0] state;
-	wire [1:0] dir;
-	
-	RateDivider r0(
-		  .cout(delay),
-		  .resetn(resetn),
-		  .clk(clk),
-		  .d(28'd2)
-	);
-	
-	control C0(
-        .clk(clk),
-        .resetn(resetn),
-		  .delay(delay),
-		  .dir(dir),
-		  
-		  
-		  .plot(plot),
-		  .direction(direction),
-		  .state(state)
-    );
-	 
-
-    datapath D0(
-        .clk(clk),
-        .resetn(resetn),
-		  .direction(direction),
-		  .state(state),
-		  
-        .x_out(x_out),
-		  .y_out(y_out),
-		  .colour(colour)
-    );
-	 
-
-	 keyboard_reader k0 (
-			.left(l),
-			.right(r),
-			.up(u),
-			.down(d),
-			.out(dir)
-	 );
-endmodule
 
 
